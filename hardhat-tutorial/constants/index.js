43e003ec8c6cf60975e5125bd4a38a3b8270ac0e
Pheranmie <tobi.okekunle@reventtechnologies.com>
2022-07-25 21:59:50 +0100

    f&las

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6fa01d3d7bdcd9c2e37ab1e396e4b532bbdb51f8";
+// URL to extract Metadata for a NFT
+const METADATA_URL = "https://whitelist-dapps-one.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
